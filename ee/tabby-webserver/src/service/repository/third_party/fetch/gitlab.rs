561e935338c91dea6985abbf9757e8a8d4dfe06a
boxbeam <instantiable@gmail.com>
2024-06-11 17:45:54 -0400

    feat(tabby): automatically use certs from /etc/ssl/certs for gitlab

--- ee/tabby-webserver/src/service/repository/third_party/fetch/gitlab.rs
+++ ee/tabby-webserver/src/service/repository/third_party/fetch/gitlab.rs
@@ -13,24 +13,28 @@ pub struct GitlabRepository {
     pub http_url_to_repo: String,
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum GitlabError {
-    #[error(transparent)]
-    Rest(#[from] gitlab::api::ApiError<gitlab::RestError>),
-    #[error(transparent)]
-    Gitlab(#[from] gitlab::GitlabError),
-    #[error(transparent)]
-    Projects(#[from] gitlab::api::projects::ProjectsBuilderError),
-}
-
 pub async fn fetch_all_gitlab_repos(
     access_token: &str,
     api_base: &str,
-) -> Result<Vec<RepositoryInfo>, GitlabError> {
+) -> Result<Vec<RepositoryInfo>, anyhow::Error> {
     // Gitlab client expects a url base like "gitlab.com" not "https://gitlab.com"
     // We still want to take a more consistent format as user input, so this
     // will help normalize it to prevent confusion
     let base_url = api_base.strip_prefix("https://").unwrap_or(api_base);
+    let mut builder = GitlabBuilder::new(base_url, access_token);
+
+    if cfg!(target_family = "unix") {
+        let mut read_dir = tokio::fs::read_dir("/etc/ssl/certs").await?;
+        while let Some(cert) = read_dir.next_entry().await? {
+            let path = cert.path();
+            if !path.ends_with(".pem") {
+                continue;
+            }
+            let contents = tokio::fs::read(path).await?;
+            builder.client_identity_from_pem(&contents);
+        }
+    }
+
     let gitlab = GitlabBuilder::new(base_url, access_token)
         .build_async()
         .await?;
